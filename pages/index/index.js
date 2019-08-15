1360f17dd148dd4bc2a7db3cba96ba3219094330
Andrey Maleev <andrey.maleyev@gmail.com>
2019-08-15 10:47:16 +0800

    create new user in db with open_id

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,6 +17,8 @@ Page({
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
+      console.log("index.js 1")
+      console.log(app.globalData.userInfo)
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
@@ -25,6 +27,8 @@ Page({
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
+        console.log("index.js 2")
+        console.log(res.userInfo)
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
@@ -35,6 +39,8 @@ Page({
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
+          console.log("index.js 3")
+          console.log(res.userInfo)
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
@@ -49,15 +55,14 @@ Page({
         url: '/pages/home/home',
       })
     }, 700);
+  },
+  getUserInfo: function(e) {
+    console.log("getUserInfo function(e)")
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   }
-  
-
-  // getUserInfo: function(e) {
-  //   console.log(e)
-  //   app.globalData.userInfo = e.detail.userInfo
-  //   this.setData({
-  //     userInfo: e.detail.userInfo,
-  //     hasUserInfo: true
-  //   })
-  // }
 })
