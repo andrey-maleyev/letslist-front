5e428edea0bc6d5024411cde32579e3aa0373da7
Yuman Xia <yumanxia@foxmail.com>
2019-08-15 19:35:00 +0800

    fixed clicked

--- utils/apiClient.js
+++ utils/apiClient.js
@@ -11,6 +11,18 @@ const getMyEvents = options => {
   })
 }
 
+const getItems = options => {
+  const { success, fail } = options
+
+  return wx.request({
+    url: baseUrl + "items",
+    method: "get",
+    success,
+    fail
+  })
+}
+
 export default {
-  getMyEvents
+  getMyEvents,
+  getItems
 }
