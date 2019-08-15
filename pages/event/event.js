ea0cccaa5877580a3fe4ab9e5388b0c996d0f3ee
Dwesmond Ho <desho8@gmail.com>
2019-08-15 18:55:41 +0800

    added Invite Particpant Page

--- pages/event/event.js
+++ pages/event/event.js
@@ -2,107 +2,63 @@
 Page({
 
   /**
-   * 页面的初始数据
+   * Page initial data
    */
   data: {
-    items: [
-      { name: 'Hamburgers', value: '美国' },
-      { name: 'Sandwiches', value: '中国' },
-      { name: 'Cake', value: '巴西' },
-      { name: 'Beef', value: '日本' },
-      { name: 'Rice', value: '英国' },
-      { name: 'Hotdogs', value: '英国' },
-      { name: 'Pancakes', value: '英国' },
-      { name: 'Sausage rolls', value: '英国' },
-      { name: 'French toasts', value: '英国' },
-      { name: 'Crispy potato chips', value: '英国'},
-      { name: 'Chicken', value: '英国' }
-    ],
-    tools: [
-      { name: 'Projector', value: '美国' },
-      { name: 'Folders', value: '中国'},
-      { name: 'Chargers', value: '巴西' },
-      { name: 'Candels', value: '日本' },
-      { name: 'Gifts', value: '英国' },
-      { name: 'Balloons', value: '英国' },
-      { name: 'Banners', value: '英国' },
-      { name: 'Party blowers', value: '英国' },
-      { name: 'Party hats', value: '英国' },
-      { name: 'Confetti', value: '英国' },
-
-    ],
-    beverage: [
-      { name: 'Cola', value: '美国' },
-      { name: 'Seven Up', value: '中国' },
-      { name: 'Milk', value: '巴西' },
-      { name: 'Water', value: '日本' },
-      { name: 'Coffee', value: '英国' },
-      { name: 'Sprite', value: '英国' },
-      { name: 'Green tea', value: '英国' },
-      { name: 'Black tea', value: '英国' },
-      { name: 'Juice', value: '英国' },
-      { name: 'Soda', value: '英国' },
-    ]
-
 
   },
-
- checkboxChange: function (e) {
-    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
-  },
-
 
   /**
-   * 生命周期函数--监听页面加载
+   * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
 
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * Lifecycle function--Called when page is initially rendered
    */
   onReady: function () {
 
   },
 
   /**
-   * 生命周期函数--监听页面显示
+   * Lifecycle function--Called when page show
    */
   onShow: function () {
 
   },
 
   /**
-   * 生命周期函数--监听页面隐藏
+   * Lifecycle function--Called when page hide
    */
   onHide: function () {
 
   },
 
   /**
-   * 生命周期函数--监听页面卸载
+   * Lifecycle function--Called when page unload
    */
   onUnload: function () {
 
   },
 
   /**
-   * 页面相关事件处理函数--监听用户下拉动作
+   * Page event handler function--Called when user drop down
    */
   onPullDownRefresh: function () {
 
   },
 
   /**
-   * 页面上拉触底事件的处理函数
+   * Called when page reach bottom
    */
   onReachBottom: function () {
 
   },
 
   /**
-   * 用户点击右上角分享
+   * Called when user click on the top right corner to share
    */
   onShareAppMessage: function () {
 
